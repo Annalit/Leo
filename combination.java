170de516287a4e633f808f780fcdd316ec4f5862
aqianner <aqianner@uwlax.edu>
2015-04-07 15:04:56 -0500

    Update combination.java

--- combination.java
+++ combination.java
@@ -1,1 +1,26 @@
 //https://leetcode.com/problems/combinations/
+public class Solution{
+    List<List<Integer>> result=new ArrayList<List<Integer>>();
+    public List<List<Integer>> combine(int n,int k){
+        List<Integer> inner = new ArrayList<Integer>();
+        int[] a = new int[n];
+        for(int i = 0; i< n; i++){
+            a[i]=i+1;
+        }
+        helper(a,inner,k);
+        return result;
+    }
+    void helper(int[] right, List<Integer> temp, int k){
+        if(temp.size()== k){
+            result.add(temp);
+            return;
+        }
+        for(int i = 0;i < right.length; i++){
+            List<Integer> templeft = new ArrayList<Integer>(temp);
+            templeft.add(right[i]);
+            int[] t=Arrays.copyOfRange(right ,i+1,right.length);
+            helper(t,templeft,k);    
+        }
+    }
+    
+}
