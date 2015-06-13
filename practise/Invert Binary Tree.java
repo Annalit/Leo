c8fdbf5d0008d28d08be1fe48d78d4cf3e48fceb
aqianner <aqianner@gmail.com>
2015-06-13 17:09:06 -0500

    Update Invert Binary Tree.java

--- practise/Invert Binary Tree.java
+++ practise/Invert Binary Tree.java
@@ -10,4 +10,9 @@ public class Solution {
 		root.right=invertTree(temp);
 		return root;
 	}
+	//exchange the left subtree and right subtree
+	1. For the current node, exchange the left child and right child.
+	2. For the left child, do it.
+	3. For the right child, do it.
+	you know how to exchange, right?
 }
