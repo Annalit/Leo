a1bb585847305a6551b7caaa91c16ab20ff5b5b9
aqianner <aqianner@gmail.com>
2015-06-17 09:49:14 -0500

    Create Populating Next Right Pointers in Each Node.java

---
+++ practise/Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        Queue<TreeLinkNode> q = new LinkedList<TreeLinkNode>();
+        if(root == null){
+            return;
+        }
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size(); //important
+            for(int i = 0; i < size; i++){
+                TreeLinkNode temp = q.poll();
+                if(i<size-1){
+                    temp.next = q.peek();
+                } else {
+                    temp.next = null;
+                }
+                if(temp.left!=null&&temp.right!=null){
+                    q.add(temp.left);
+                    q.add(temp.right);
+                }
+            }
+        }
+    }
+}
