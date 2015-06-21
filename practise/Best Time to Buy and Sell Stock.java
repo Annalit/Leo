641b2ee3405811f020069d8d939ecbd9232aaef8
aqianner <aqianner@gmail.com>
2015-06-21 10:30:15 -0500

    Update Best Time to Buy and Sell Stock.java

--- practise/Best Time to Buy and Sell Stock.java
+++ practise/Best Time to Buy and Sell Stock.java
@@ -7,9 +7,10 @@ public class Solution {
         int max = 0;
         int min = prices[0];
         for(int i = 1; i <length; i++){
-            max = Math.max(max, prices[i] - min);
+            max = Math.max(max, prices[i] - min);// don't compare with the last one, compare with the most one;
             min = Math.min(min, prices[i]);
         }
+        // the answer just depends on the max and the min, so get the formula for them, and store them.
         return max;
     }
 }
