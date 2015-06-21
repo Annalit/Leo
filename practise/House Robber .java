9bc3e32bb5ec80bd5e9537cce33377c8e4cc122a
aqianner <aqianner@gmail.com>
2015-06-20 22:31:21 -0500

    Create House Robber .java

---
+++ practise/House Robber .java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int rob(int[] nums) {
+     //maxV[i] = max(maxV[i-2]+num[i], maxV[i-1])
+        if(nums.length == 0){
+            return 0;
+        }
+        int length = nums.length;
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        res.add(nums[0]);
+        
+        if(length>1)
+            res.add(Math.max(res.get(0), nums[1]));
+        for(int i = 2; i < length; i++){
+            res.add(Math.max(res.get(i-1), nums[i]+res.get(i-2)));
+        }
+        return res.get(length-1);
+    }
+}
