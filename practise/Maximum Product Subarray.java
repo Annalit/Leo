eb4af354a81feff4b75375fa02f722c93c2da916
aqianner <aqianner@gmail.com>
2015-06-21 22:39:15 -0500

    Update Maximum Product Subarray.java

--- practise/Maximum Product Subarray.java
+++ practise/Maximum Product Subarray.java
@@ -16,3 +16,23 @@ public class Solution {
         return max;
     }
 }
+
+public class Solution {
+    public int maxProduct(int[] nums) {
+        int len = nums.length;
+        if(nums == null|| len==0){
+            return 0;
+        }
+        int max = nums[0];
+        int[] minlocal = new int[len];
+        int[] maxlocal = new int[len];
+        minlocal[0] = nums[0];
+        maxlocal[0] = nums[0];
+        for(int i = 1; i < len; i++){
+            maxlocal[i] = Math.max(maxlocal[i-1]*nums[i], Math.max(nums[i],minlocal[i-1]*nums[i]));
+            minlocal[i] = Math.min(maxlocal[i-1]*nums[i], Math.min(nums[i],minlocal[i-1]*nums[i]));
+            max = Math.max(max, maxlocal[i]);
+        }
+        return max;
+    }
+}
