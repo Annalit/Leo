7fd0962ff9b9de6df6ba57b70bc2cee236e79c1c
aqianner <aqianner@gmail.com>
2015-06-25 13:48:28 -0500

    Update Word Break.java

--- practise/Word Break.java
+++ practise/Word Break.java
@@ -23,3 +23,33 @@ public class Solution {
             return false;
         }
 }
+second time
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        int len = s.length();
+        Boolean[][] check = new Boolean[len][len];
+        if(wordDict==null||wordDict.isEmpty())
+            return false;
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                check[i][j] =false;
+            }
+        }
+        for(int i = 0; i < len; i++){
+            check[i][i] = wordDict.contains(s.substring(i,i+1));
+        }
+        for(int i = 1; i < len; i++){
+            for(int j = i; j < len; j++){
+                for(int k = j - i; k < j; k++){
+                		Boolean a = (wordDict.contains(s.substring(j-i,j+1)));
+                		Boolean b = (check[j-i][k]&&check[k+1][j]);
+           
+                        check[j-i][j] =check[j-i][j]||(wordDict.contains(s.substring(j-i,j+1)))||(check[j-i][k]&&check[k+1][j]);
+                }
+            }
+        }
+        return check[0][len-1];
+    }
+}
