e5de40670eee8720b71d7365aacf8659f722a8dc
aqianner <aqianner@gmail.com>
2015-06-26 10:45:27 -0500

    Create N-Queens.java

---
+++ practise/N-Queens.java
@@ -0,0 +1,55 @@
+TLE
+public class Solution {
+    ArrayList<List<String>> result = new ArrayList<List<String>>();
+    public List<List<String>> solveNQueens(int n) {
+        ArrayList<String> inner = new ArrayList<String>();
+        HashSet<Integer> sum = new HashSet<Integer>();
+        HashSet<Integer> dif = new HashSet<Integer>();
+        HashSet<Integer> col = new HashSet<Integer>();
+        helper(inner,0, n, sum, dif,col);
+        return result;
+    }
+    void helper(ArrayList<String> inner, int q, int n,  HashSet<Integer> sum, HashSet<Integer> dif,HashSet<Integer> col){      
+        int len = inner.size()-1;
+        if(len == 3){
+        	int a= 1;
+        }
+        if(sum.contains(q+len)||dif.contains(q-len)||col.contains(q)){
+        	if(q==n-1){
+        		 sum.clear();
+                 dif.clear();
+                 col.clear();
+        	}
+            return;
+        } else if(len == n-1&&(!inner.isEmpty())){
+            result.add(inner);
+        	if(len==n-1&&q==n-1){
+       		 	sum.clear();
+                dif.clear();
+                col.clear();
+        	}
+            return;
+        } else if(!inner.isEmpty()){
+            sum.add(q+len);
+            dif.add(q-len);
+            col.add(q);
+        }
+        for(int i = 0; i < n;i++){    
+            ArrayList<String> innertemp = new ArrayList<String>(inner);
+            String temp = createString(i, n);
+            innertemp.add(temp);
+            helper(innertemp, i, n, sum, dif, col);
+        }
+    }
+    String createString(int q, int n){
+        StringBuffer sb = new StringBuffer(n);
+        for(int i = 0; i < n; i++){
+            if(i== q){
+                sb.append("Q");
+            } else {
+                sb.append(".");
+            }
+        }
+        return sb.toString();
+    }
+}
