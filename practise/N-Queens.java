8dbf28d7748ea07ecf19b87b2606e96507ba2f64
aqianner <aqianner@gmail.com>
2015-06-26 11:23:33 -0500

    Update N-Queens.java

--- practise/N-Queens.java
+++ practise/N-Queens.java
@@ -98,3 +98,50 @@ public class Solution {
         return sb.toString();
     }
 }
+
+good solution
+public class Solution {
+    ArrayList<List<String>> result = new ArrayList<List<String>>();
+    public List<List<String>> solveNQueens(int n) {
+        ArrayList<String> inner = new ArrayList<String>();
+        helper(inner,0, n);
+        return result;
+    }
+    void helper(ArrayList<String> inner, int q, int n){      
+        int len = inner.size()-1;
+        if(!check(inner,q)){
+            return;
+        } else if(len == n-1&&(!inner.isEmpty())){
+            result.add(inner);
+            return;
+        } 
+        for(int i = 0; i < n;i++){    
+            ArrayList<String> innertemp = new ArrayList<String>(inner);
+            String temp = createString(i, n);
+            innertemp.add(temp);
+            helper(innertemp, i, n);
+        }
+    }
+    String createString(int q, int n){
+        StringBuffer sb = new StringBuffer(n);
+        for(int i = 0; i < n; i++){
+            if(i== q){
+                sb.append("Q");
+            } else {
+                sb.append(".");
+            }
+        }
+        return sb.toString();
+    }
+    private boolean check(ArrayList<String> inner, int q){
+    	int len = inner.size()-1;
+    	for(int i = 0; i < inner.size()-1; i++){
+    		int r = inner.get(i).indexOf("Q");
+    		int l = i;
+    		if(len+q==r+l||len-q==l-r||q==r){
+    			return false;
+    		} 
+    	}
+    	return true;          
+    }
+}
