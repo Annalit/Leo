4ce43d0d07f2c7ec005cac55823ef6c934285be9
aqianner <aqianner@gmail.com>
2015-06-30 11:41:31 -0500

    Update Jump Game II.java

--- practise/Jump Game II.java
+++ practise/Jump Game II.java
@@ -1,3 +1,4 @@
+TLE
 public class Solution {
     public int jump(int[] nums) {
         Queue<Integer> q = new LinkedList<Integer>();
@@ -23,6 +24,45 @@ public class Solution {
             }
             if(flag)
                 break;
+        }
+        return count;
+    }
+}
+beautiful code
+这里没有想清楚的又是边界条件，
+[0],
+[1,2]
+这里是很容易出问题的，不够还是因为循环定义脑子里不清晰。
+画一棵树就知道
+---------2
+-------5---4
+-----1-3-4-5-6
+每个节点的值表示它能走的最远距离，也就是以它为root的时候它的孩子的个数，也就是树的宽度
+一开始想用queue.size()来保存层，但是其实你会发现每一层的宽度不就是该层的最右点和上层的最右点只差么。。
+
+什么叫一个节点的孩子，就是这个节点能够一次接触到的下一个节点。。
+当前层宽度的右节点用curmax表示。
+下一层宽度的右节点用max表示。
+在当前层完全走完之前，下一层的宽度是一直在调整的。
+显然当这棵树只有一个节点的时候max 和 curmax 为0；
+什么时候count++？count是什么，count是层数。
+当从2跳到5的时候，以及4跳到1的时候，也就是说我的当前层发生改变的时候
+也就是i = curmax+1的时候。
+此时curmax显然马上被赋值为max。所以这道题里面max有一种记录树的宽度的神奇妙用，就像queue的size一样。
+其实下一层的宽度是上一层的最大值，
+其实这个地方len不一定能到max，max就像一个完全树一样，不过count记录了它的高度就够了。
+public class Solution {
+    public int jump(int[] nums) {
+        int len = nums.length;
+        int upper_breadth = 0;
+        int max = 0;
+        int count = 0;
+        for(int i = 0; i < len; i++){
+            if(i > upper_breadth){
+                count++;
+                upper_breadth = max;
+            }
+            max = Math.max(max, nums[i] + i);
         }
         return count;
     }
