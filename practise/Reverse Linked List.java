6e6bce000129b2eee56ea6c975e30d11140a7211
aqianner <aqianner@gmail.com>
2015-07-13 11:23:58 -0500

    Update Reverse Linked List.java

--- practise/Reverse Linked List.java
+++ practise/Reverse Linked List.java
@@ -26,3 +26,19 @@ public class Solution {
         return p;
     }
 }
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode fakehead = new ListNode(-1);
+        ListNode cur = new ListNode(-1);
+        cur.next = head;
+        while (cur != null && cur.next != null){
+            ListNode temp = cur.next.next;
+            cur.next.next = fakehead.next;
+            fakehead.next = cur.next;
+            cur.next = temp;
+        }
+        return fakehead.next;
+    }
+}
+下面这个代码会简洁很多，因为其实就像插入排序一样，其实弄了两个链表，然后把后面的链表一点点调整到前面的链表来，这种方法好好理解哈。
+不要把fakehead.next 给随便赋值了！
