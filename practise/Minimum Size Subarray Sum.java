ab8fc867a8b871655ecf2bf38ece8c1b281be6d0
aqianner <aqianner@gmail.com>
2015-07-22 14:57:55 -0500

    Update Minimum Size Subarray Sum.java

--- practise/Minimum Size Subarray Sum.java
+++ practise/Minimum Size Subarray Sum.java
@@ -16,3 +16,28 @@ public class Solution {
         return min;
     }
 }
+别人的代码，比我的快太多。。
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int len = nums.length;
+        int p = 0, q = 0, cur = 0, min = Integer.MAX_VALUE;
+        while (p < len) {
+            cur = nums[q];
+            while (cur < s && q < len - 1) {
+                q++;
+                cur +=nums[q];
+            }
+            if (cur >= s)
+                min = Math.min(q - p + 1, min);
+                if (min == 1) {
+                    return min;
+                }
+            p++;
+            q = p;
+        }
+        if (min == Integer.MAX_VALUE) {
+            return 0;
+        }
+        return min;
+    }
+}
