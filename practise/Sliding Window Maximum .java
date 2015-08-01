dd1cfe19c54e65aa3f5f23202162f93271f8a391
aqianner <aqianner@gmail.com>
2015-07-31 21:59:38 -0500

    Create Sliding Window Maximum .java

---
+++ practise/Sliding Window Maximum .java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int len = nums.length;
+        int rol = len - k + 1;
+        int[] res = new int[rol];
+        int p = 0;
+        int temp = 0;
+        while (p < len){
+            if (queue.isEmpty() || nums[queue.peekLast()] >= nums[p]) {
+                queue.offerLast(p);
+            } else {
+            	while (!queue.isEmpty()&&nums[queue.peekLast()] < nums[p]) {
+            		queue.pollLast();
+            	}
+            	queue.offerLast(p);
+            }
+            if (p - temp + 1 == k) {
+                int first = queue.peekFirst();
+                res[temp] = nums[first];
+                if (nums[temp] == nums[first]) {
+                    queue.pollFirst();
+                }
+                temp++;
+            }
+            p++;
+        }
+        return res;
+    }
+}
