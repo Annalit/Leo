19b2c8bec8b1580109f0b2b123dd3a093f2dc0ea
aqianner <aqianner@gmail.com>
2015-08-15 17:19:25 -0500

    Update generateParent.java

--- generateParent.java
+++ generateParent.java
@@ -63,3 +63,74 @@ public class Solution {
 		temp.set(5, temp.get(5) - 1);
 
 	}
+	void helper(ArrayList<Integer> temp, StringBuffer sb, Stack<Character> s) {
+		// temp = {lleft,lright,mleft,mright,nleft, nright};
+		int lleft = temp.get(0);
+		int lright = temp.get(1);
+		int mleft = temp.get(2);
+		int mright = temp.get(3);
+		int nleft = temp.get(4);
+		int nright = temp.get(5);
+		if (lleft > lright || mleft > mright || nleft > nright || lleft < 0
+				|| mleft < 0 || nleft < 0)
+			return;
+		if (lleft == lright && mleft == mright && nleft == nright && lleft == 0
+				&& mleft == 0 && nleft == 0) {
+			result.add(sb.toString());
+			return;
+		}
+		sb.append('(');
+		temp.set(0, temp.get(0) - 1);
+		s.push('(');
+		helper(temp, sb, s);
+		sb.deleteCharAt(sb.length() - 1);
+		s.pop();
+		temp.set(0, temp.get(0) + 1);
+
+		sb.append('[');
+		temp.set(2, temp.get(2) - 1);
+		s.push('[');
+		helper(temp, sb, s);
+		sb.deleteCharAt(sb.length() - 1);
+		s.pop();
+		temp.set(2, temp.get(2) + 1);
+
+		sb.append('{');
+		temp.set(4, temp.get(4) - 1);
+		s.push('{');
+		helper(temp, sb, s);
+		sb.deleteCharAt(sb.length() - 1);
+		s.pop();
+		temp.set(4, temp.get(4) + 1);
+		if (!s.isEmpty() && s.peek() == '(') {
+			sb.append(')');
+			temp.set(1, temp.get(1) - 1);
+			s.pop();
+			helper(temp, sb, s);
+			s.push('(');
+			sb.deleteCharAt(sb.length() - 1);
+			temp.set(1, temp.get(1) + 1);
+			return;
+		}
+		if (!s.isEmpty() && s.peek() == '[') {
+			sb.append(']');
+			temp.set(3, temp.get(3) - 1);
+			s.pop();
+			helper(temp, sb, s);
+			s.push('[');
+			sb.deleteCharAt(sb.length() - 1);
+			temp.set(3, temp.get(3) + 1);
+			return;
+		}
+		if (!s.isEmpty() && s.peek() == '{') {
+			sb.append('}');
+			temp.set(5, temp.get(5) - 1);
+			s.pop();
+			helper(temp, sb, s);
+			s.push('{');
+			sb.deleteCharAt(sb.length() - 1);
+			temp.set(5, temp.get(5) + 1);
+			return;
+		}
+
+	}
