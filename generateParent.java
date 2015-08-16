ec62b12a5f68cde6ef680deba2b18b389f786c3b
aqianner <aqianner@gmail.com>
2015-08-15 19:33:21 -0500

    Update generateParent.java

--- generateParent.java
+++ generateParent.java
@@ -134,3 +134,75 @@ public class Solution {
 		}
 
 	}
+	
+	public class Solution {
+	ArrayList<String> result = new ArrayList<String>();
+
+	List<String> brackets(int l, int m, int n) { // l : (), m: [], n: {}
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		temp.add(l);
+		temp.add(l);
+		temp.add(m);
+		temp.add(m);
+		temp.add(n);
+		temp.add(n);
+		helper(temp, new StringBuffer(), ')');
+		return result;
+	}
+
+	void helper(ArrayList<Integer> temp, StringBuffer sb, char last) {
+		// temp = {lleft,lright,mleft,mright,nleft, nright};
+		int lleft = temp.get(0);
+		int lright = temp.get(1);
+		int mleft = temp.get(2);
+		int mright = temp.get(3);
+		int nleft = temp.get(4);
+		int nright = temp.get(5);
+		if (lleft > lright || mleft > mright || nleft > nright || lleft < 0
+				|| mleft < 0 || nleft < 0)
+			return;
+		if (lleft == lright && mleft == mright && nleft == nright && lleft == 0
+				&& mleft == 0 && nleft == 0) {
+			result.add(sb.toString());
+			return;
+		}
+		sb.append('(');
+		temp.set(0, temp.get(0) - 1);
+		helper(temp, sb,last);
+		sb.deleteCharAt(sb.length() - 1);
+		temp.set(0, temp.get(0) + 1);
+
+		sb.append('[');
+		temp.set(2, temp.get(2) - 1);
+		helper(temp, sb, '[');
+		sb.deleteCharAt(sb.length() - 1);
+		temp.set(2, temp.get(2) + 1);
+
+		sb.append('{');
+		temp.set(4, temp.get(4) - 1);
+		helper(temp, sb, '{');
+		sb.deleteCharAt(sb.length() - 1);
+		temp.set(4, temp.get(4) + 1);
+		if (last == '(') {
+			sb.append(')');
+			temp.set(1, temp.get(1) - 1);
+			helper(temp, sb, ')');
+			sb.deleteCharAt(sb.length() - 1);
+			temp.set(1, temp.get(1) + 1);
+		}
+		if (last == '[') {
+			sb.append(']');
+			temp.set(3, temp.get(3) - 1);
+			helper(temp, sb, ']');
+			sb.deleteCharAt(sb.length() - 1);
+			temp.set(3, temp.get(3) + 1);
+		}
+		if (last == '{') {
+			sb.append('}');
+			temp.set(5, temp.get(5) - 1);
+			helper(temp, sb, '}');
+			sb.deleteCharAt(sb.length() - 1);
+			temp.set(5, temp.get(5) + 1);
+		}
+
+	}
