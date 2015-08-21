42249195efad191fdb28eac4de4838863411542f
aqianner <aqianner@gmail.com>
2015-08-20 19:43:29 -0500

    Update Kth Smallest Number in Sorted Matrix.java

--- practise/Kth Smallest Number in Sorted Matrix.java
+++ practise/Kth Smallest Number in Sorted Matrix.java
@@ -1,6 +1,28 @@
 Kth Smallest Number in Sorted Matrix过了，好像也没有多复杂，不过我发现priorityqueue要经常注意重复，不能加重复的数进去，当然如果pair本身不等，只是优先度相等，这个是允许的。
 然后就是这道题写hashset因为是自定义的类，所以要给它设计hashcode和equal方法。
 研究一下怎么写hashcode和equal，难道必须按照eclipse里面自动生成的那种方法来么。
+刚刚发现其实这样写就可以了：
+class Pair{
+    @Override
+	public int hashCode() {
+		return row * col;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		Pair other = (Pair)(obj);
+		return this.row == other.row && this.col == other.col;
+	}
+	int row, col;
+    public Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+重点是这两个一定要一起写，
+因为只写hashcode不写equal肯定不准确，除非字符串那种，一般你很难真的用hashcode来代替equal
+只写equal不写hashcode肯定错。。因为object默认的hashcode是一个object一个distinc码。。hashcode不等的话根本到不了equal那里去。。
+http://www.programcreek.com/2013/09/java-hashcode-equals-contract-set-contains/
+上面这个链接写的很好
 public class Solution {
     /**
      * @param matrix: a matrix of integers
