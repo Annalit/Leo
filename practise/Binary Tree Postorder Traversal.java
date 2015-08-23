3f34a4664973677700d10eba00c6035d06cc4d87
aqianner <aqianner@gmail.com>
2015-08-23 13:40:30 -0500

    Update Binary Tree Postorder Traversal.java

--- practise/Binary Tree Postorder Traversal.java
+++ practise/Binary Tree Postorder Traversal.java
@@ -71,5 +71,55 @@ public class Solution {
         }
         return res;
     }
+}dddpublic class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Postorder in ArrayList which contains node values.
+     * 除了用hashset，我们会发现其实我们并不需要真的记录所有的节点。
+     * 我们用hashset的本质是看从右边回来的时候，根节点是不是第二次访问。
+     * 如果用一个last来表示它是很困难的，因为如果last记录根节点的话，
+     * 走到右子树里面这个信息是可能会被丢失的。
+     * 我peek这个元素的时候到底是不是第二次peek
+     * 因为peek肯定是右子树访问完了然后peek，所以其实我们只要能确定root
+     * 的右节点是否被访问过，或者说，root的有节点是否为空，我们就可以确定root
+     * 是否需要被pop
+     * 这里面其实一开始会担心在右子树里面遍历的时候会把last的信息给丢掉，
+     * 但是这里last的赋值有没有规律呢。
+     * 也就是一个节点被pop之后，赢让last指向这个节点
+     * last永远指向我准备出栈时，栈顶元素的右孩子
+     * 出栈之后，如果当前子树是左子树，last赋值其实无所谓，但是如果是右子树，last需要指向该pop()元素，
+     * 因为其实last元素按照一种出栈顺序指向peek()元素的右孩子，其实postorder是一种从爸爸到孩子的压栈，那么每次右孩子
+     * 可以pop出来的时候，
+     * 说明父亲一定已经被peek()过一次，因为我们每次都是通过父节点来找右孩子，不然右孩子不会被压栈的对不对~
+     * 所以每次root节点确定可以被pop的时候，它需要还原的last就是它本身。
+     * 当然如果root节点是某个节点的左孩子，其实这个就无所谓了啦。
+     * 为什么这里用last来存peek()的右孩子呢。要是存peek()本身来表示上次被访问的孩子呢
+     * 哈哈那是因为如果存右孩子，我可以让last从孩子到父亲赋值
+     * 但是如果是寸节点本身，我这个节点被pop之后，我的last怎么赋值才好呢，我需要指向它的parent呀~~~
+     */
+     
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        TreeNode cur = root;
+        TreeNode last = new TreeNode(-1);
+        while (!s.isEmpty() || cur != null) {
+            if (cur == null) {
+                TreeNode temp = s.peek();
+                if (temp.right == last || temp.right == null) {
+                    res.add(temp.val);
+                    last = temp;
+                    s.pop();
+                } else {
+                    cur = temp.right;
+                }
+            } else {
+                s.push(cur);
+                cur = cur.left;
+            }
+        }
+        return res;
+    }
 }
 // better to define TreeNode cur = root and then use cur instead.
