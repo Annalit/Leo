3048e10f30d12923adf1b7ef7eda6ad1f89708aa
aqianner <aqianner@gmail.com>
2015-08-22 19:40:54 -0500

    Update Binary Tree Preorder Traversal.java

--- practise/Binary Tree Preorder Traversal.java
+++ practise/Binary Tree Preorder Traversal.java
@@ -30,3 +30,33 @@ public class Solution {
         return result;
     }
 }
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+        //把所有的right节点全部存在一个stack里面，然后什么时候pop呢
+        //当cur为空的时候！我们使cur = stack.pop()，重新循环就好了！
+        //什么时候结束循环，stack为空，可是一开始就是空，但是这个时候的cur
+        //不是空，所以应该是cur为空而且stack也为空的时候。
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        if (root == null) {
+            return res;
+        }
+        TreeNode cur = root;
+        while(cur != null || !s.isEmpty()) {
+            if (cur == null) {
+                cur = s.pop();
+            }
+            res.add(cur.val);
+            if (cur.right != null) {
+                s.push(cur.right);
+            }
+            cur = cur.left;
+        }
+        return res;
+    }
+}
