2298da51639586ba3ea136fd6da8d3d36273c32b
aqianner <aqianner@gmail.com>
2015-10-02 20:33:39 -0500

    Update Word Search.java

--- practise/Word Search.java
+++ practise/Word Search.java
@@ -161,3 +161,103 @@ public class Solution {
             tb[row][col]=keep;// you only need to return to the original when there is really no way to go!!!!
     }
 }
+下面这个代码真是漂亮。
+不用flag。
+写深度优先的时候要牢记，递归的终止条件，以及什么时候需要递归。
+比如这里，越界了是不用判断的。
+然后如果当前字符不相等是不用判断的。
+在这基础上，如果当前字符相等，是不是就一定要进入下一步判断呢。
+如果长度已经到了length - 1了，自然也不用判断了。
+这里一开始的写法是用的if (count == length) 然后退出。。
+这里其实逻辑不太好，因为我并不是任何情况下都应该进入下一步递归的。。如果当前的相等，然后发现当前的就是最后一位了，
+其实就不应该进入下一步递归了。。其实那样写也可以，只是要判断的东西多了一点。。
+
+
+public boolean exist(char[][] board, String word) {
+    int m = board.length;
+    int n = board[0].length;
+ 
+    boolean result = false;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+           if(dfs(board,word,i,j,0)){
+               result = true;
+           }
+        }
+    }
+ 
+    return result;
+}
+ 
+public boolean dfs(char[][] board, String word, int i, int j, int k){
+    int m = board.length;
+    int n = board[0].length;
+ 
+    if(i<0 || j<0 || i>=m || j>=n){
+        return false;
+    }
+ 
+    if(board[i][j] == word.charAt(k)){
+        char temp = board[i][j];
+        board[i][j]='#';
+        if(k==word.length()-1){
+            return true;
+        }else if(dfs(board, word, i-1, j, k+1)
+        ||dfs(board, word, i+1, j, k+1)
+        ||dfs(board, word, i, j-1, k+1)
+        ||dfs(board, word, i, j+1, k+1)){
+            return true;
+        }
+        board[i][j]=temp;
+    }
+    
+    public class Solution {
+    boolean flag = false;
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                helper(i, j, board, word, 0);
+                if (flag) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    void helper(int row, int col, char[][] board, String word, int count) {
+        if (flag ||(board[row][col] == '*')) {
+            return;
+        }
+        if (board[row][col] != word.charAt(count)) {
+            return;
+        }
+        if (count == word.length() - 1) {
+            flag = true;
+            return;
+        }
+            char temp = board[row][col];
+            if (row > 0) {
+                board[row][col] ='*';
+                helper(row - 1, col, board, word, count + 1);
+                board[row][col] =temp;
+            } 
+            if (col > 0) {
+                board[row][col] ='*';
+                helper(row, col - 1, board, word, count + 1);
+                board[row][col] =temp;
+            }
+            if (row < board.length - 1) {
+                board[row][col] ='*';
+                helper(row + 1, col, board, word, count + 1);
+                board[row][col] =temp;
+            }
+            if (col < board[row].length - 1) {
+                board[row][col] ='*';
+                helper(row, col + 1, board, word, count + 1);
+                board[row][col] =temp;
+            }
+    }
+}
+ 
+    return false;
+}
